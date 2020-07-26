1b147aab4c02ada9470085789db6275afae4a7f0
dylan petty <dylanpetty70@yahoo.com>
2020-07-26 12:08:17 -0500

    adding 404 page

--- src/Router.js
+++ src/Router.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import Home from "./components/Home";
 import Investments from "./components/Investments";
 import Foresight from "./components/Foresight";
 import Scanners from "./components/Scanners";
 import Login from "./components/Login";
+import RedirectPage from "./components/404";
 
 
 class Router extends Component {
@@ -17,6 +18,8 @@ class Router extends Component {
                 <Route path="/foresight" component={Foresight} />
                 <Route path="/scanners" component={Scanners} />
                 <Route path="/login" component={Login} />
+                <Route path="/why-are-you-trying-to-break-my-site" component={RedirectPage} />
+                <Redirect from="/*" to="/why-are-you-trying-to-break-my-site" />
         </Switch>
         </div>
       );
